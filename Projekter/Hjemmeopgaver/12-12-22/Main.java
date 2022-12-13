a0f3b02ecb6b3473928cc44b7b731f215d583063
StilosGH <workmailchristian@gmail.com>
2022-12-13 01:01:44 +0100

    MVP

--- Projekter/Hjemmeopgaver/12-12-22/Main.java
+++ Projekter/Hjemmeopgaver/12-12-22/Main.java
@@ -2,19 +2,17 @@ public class Main {
     public static void main(String[] args) {
         int[] t = {3, 1, 2, 5, 5, 1, 6, 1, 2, 5, 4};
         int[] target = {1, 2, 5};
-        checkArray(t, target);
+        int[] replace = {3, 1, 2};
+        //checkArray(t, target);
+        replaceArray(t, target, replace);
     }
 
     public static void checkArray(int [] array, int [] target) {
         for (int i = 0; i < array.length - target.length; i++) {
             if (array[i] == target[0]) {
-                int skipIndexes = 0;
                 boolean found = false;
                 for (int j = 1; j < target.length; j++) {
-                    skipIndexes++;
                     if (array[i+j] != target[j]) {
-                        skipIndexes--;
-                        i += skipIndexes;
                         break;
                     }
                     found = true;
@@ -22,8 +20,39 @@ public class Main {
                 if (found) {
                     System.out.println("target found @ [" + i + "]");
                 }
-                i += skipIndexes;
             }   
+        }
+    }
+
+    public static void replaceArray(int [] array, int [] target, int [] replace) {
+        for (int i = 0; i < array.length - target.length; i++) {
+            if (array[i] == target[0]) {
+                boolean found = false;
+                for (int j = 1; j < target.length; j++) {
+                    if (array[i+j] != target[j]) {
+                        break;
+                    }
+                    found = true;
+                }
+                if (found) {
+                    for (int k = 0; k < replace.length; k++) {
+                        array[i + k] = replace[k];
+                        System.out.print(array[i + k]);
+                    }
+                    i += replace.length;
+                }
+            }
+        }   
+        
+
+        for (int i : array) {
+            System.out.print("| " + i + " |");
+        }
+    }
+
+    public static void replaceWith(int []array, int [] replace) {
+        for (int i = 0; i < replace.length; i++) {
+            array[i] = replace[i];
         }
     }
 }
